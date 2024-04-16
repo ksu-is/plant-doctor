2b3332b80032cfba9c759579f9fd31116ccb802b
ejorda44 <156045992+ejorda44@users.noreply.github.com>
2024-04-16 13:34:53 -0400

    finishing gui (for now)
    i am done with the gui for now! i had to give up on adding the bg image. but i added padding and finished the general layout. now i will begin on the recommendations function. once that function is complete, i will finish the last frame of the gui

--- plant-doctor-gui-GRID.py
+++ plant-doctor-gui-GRID.py
@@ -6,6 +6,8 @@ from customtkinter import *
 from PIL import Image
 from PIL import Image, ImageTk
 
+
+# defining color varialbes
 tan = '#DDD0B6'
 dtan = '#BFAF94'
 lgreen = '#3D5535'
@@ -14,13 +16,11 @@ dgreen = '#2F3D2A'
 app = ctk.CTk(fg_color = tan)
 
 #configuring the grid
-
 app.columnconfigure(0, weight=1) # row 1
 app.columnconfigure(1, weight=1) # row 2
 app.columnconfigure(2, weight=1) # row 3
 
 # creating variables with font info to make it easer to style the text
-
 h1 = ctk.CTkFont(family="Blue_Cashews", size=32)
 h2 = ctk.CTkFont(family="Abril Display", size=28)
 h2 = ctk.CTkFont(family="Abril Display", size=24)
@@ -28,11 +28,19 @@ body = ctk.CTkFont(family="DM Sans Medium", size=16)
 buttontxt = ctk.CTkFont(family="DM Sans Bold", size=16)
 
 # page that says hello and asks for your name
-
 welcome = ctk.CTkFrame(
     app,
-    fg_color = tan)
+    width = 800,
+    height = 600,
+    fg_color = 'transparent')
 welcome.grid(row=0, column=0, sticky="nsew")
+
+# creating variable to use as the bg image on the WELCOME page; not working but im saving it just in case
+welcome_bg_img = CTkImage(
+    Image.open("logo-pngs/bg-img.jpg"),
+    size=(800,600)
+)
+welcome_bg_img_label = ctk.CTkLabel(app, image=welcome_bg_img)
 
 # page that welcomes you by name and asks for user input
 input = ctk.CTkFrame(
@@ -41,20 +49,21 @@ input = ctk.CTkFrame(
 input.grid(row=0, column=0, sticky="nsew")
 
 # page that displays recommendations based on user input
-
 recommendation = ctk.CTkFrame(
     app,
     fg_color = tan)
 recommendation.grid(row=0, column=0, sticky="nsew")
 
 # defining logo variables for whole app
-
 welcome_logo = CTkImage(
     light_image=Image.open("logo-pngs/PlantDoctor_Logo-06.png"),
     dark_image=Image.open("logo-pngs/PlantDoctor_Logo-06.png"),
-    size=(200,105)
+    size=(200,105),
     )
-welcome_logo_label = ctk.CTkLabel(welcome, image=welcome_logo, text = "")
+welcome_logo_label = ctk.CTkLabel(
+    welcome,
+    image=welcome_logo,
+    text = "")
 
 input_logo = CTkImage(
     light_image=Image.open("logo-pngs/PlantDoctor_Logo-06.png"),
@@ -72,8 +81,9 @@ recommendation_logo_label = ctk.CTkLabel(recommendation, image=recommendation_lo
 
 # WELCOME PAGE
 # title of welcome page
-
-welcome_logo_label.grid()
+welcome_logo_label.grid(
+    pady = (120,0)
+) # the plant doctor logo
 
 welcome_title = ctk.CTkLabel(
     welcome,
@@ -86,9 +96,7 @@ welcome_title = ctk.CTkLabel(
 welcome_title.grid()
 
 # entry box for username
-
 name_var = tk.StringVar()
-
 username = ctk.CTkEntry(
     welcome,
     textvariable = name_var,
@@ -105,12 +113,10 @@ username = ctk.CTkEntry(
 username.grid()
 
 # function to update the input_title variable to include the user's name
-
 def update_input_title():
     input_title.configure(text="Hello, " + name_var.get().capitalize() + "!\nWhat is going on with your plant?")
 
 # function to move to input page & update the input_title variable
-
 def to_input():
     update_input_title()
     input.tkraise()
@@ -135,19 +141,18 @@ enter.grid(
 
 # INPUT PAGE
 # title of input page
-
 input_title = ctk.CTkLabel(
     input,
     text="",
     font=h1,
     text_color = lgreen,
     width=800,
-    pady=20,
     wraplength=600)
-input_title.grid()
+input_title.grid(
+    pady=(100, 10)
+)
 
 # symptom entry box
-
 symptomentry = ctk.CTkEntry(
     input,
     placeholder_text = "Describe your plant's symptoms here...",
@@ -187,7 +192,6 @@ to_rec_button.grid(
 
 # RECOMMENDATION PAGE
 # recommendations page title
-
 recommendations_title = ctk.CTkLabel(
     recommendation,
     text=("Here are some recommendations:"),
